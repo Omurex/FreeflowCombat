35b8cd718ae4d0667227f333ff7376ed91be77ac
Omurex <josephthomaslyons@gmail.com>
2023-05-09 23:30:32 -0400

    Fine tuned some stuff and got controller controls working better

--- FreeflowCombat/Assets/Input/MainInput.cs
+++ FreeflowCombat/Assets/Input/MainInput.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b5693a1fcbe8d51e3a980839915f1348e8d64639d1b562c2412052e90b9b80d
-size 13451
+oid sha256:efc6118b4947e5a7d1e5e13bde1e20a3391f5e893a0214bd0e19619c1fec22f1
+size 13468
---
+++ FreeflowCombat/Assets/Scripts/Test/Test_LookAtObject.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:abaa4e2bde71c2b9289365f5f7753168743c14cb62f4e0a7e74f53526aa01514
+size 358
