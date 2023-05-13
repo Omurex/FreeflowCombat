d2b1ea8ca4f0ecefe4755abb746d004e62d675fd
Omurex <josephthomaslyons@gmail.com>
2023-05-13 17:47:33 -0400

    Started work getting second attack animation in

--- FreeflowCombat/Assets/Scripts/Character/Player/PlayerController.cs
+++ FreeflowCombat/Assets/Scripts/Character/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f615969e3051732c0e2915fdf48147682d84406c3d098da558492b3e74fc4a93
-size 15888
+oid sha256:959c03920fa96607ddd6ad128abae0c7f101d4e7a73dd0b05c8eec3d85d213ab
+size 15906
