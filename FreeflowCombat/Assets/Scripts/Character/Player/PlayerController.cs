bd2c24dd59cf8be9e2c7914e3757f2810f97d929
Omurex <josephthomaslyons@gmail.com>
2023-05-13 13:30:41 -0400

    Fixed bug where attack animation kept playing past attack

--- FreeflowCombat/Assets/Scripts/Character/Player/PlayerController.cs
+++ FreeflowCombat/Assets/Scripts/Character/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:148ee1c8d51626e02772ae4271294a5bc0ef21b937dbc678a7bb029145a9b602
-size 14753
+oid sha256:381adb2ca3a25b3418a313ff52ce55535a545b6fd0bda70a0c573a5459962b21
+size 15053
