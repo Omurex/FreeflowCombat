cee983528729a256e999a8ee99df3d14b121b177
Omurex <josephthomaslyons@gmail.com>
2023-05-15 15:25:12 -0400

    Made attack animations have range where they trigger

---
+++ FreeflowCombat/Assets/Scripts/Animation/AttackAnimationBundle.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:537e182b595d1247c71a606c7390e13c00df4150deaea9377099d90ee6c42e05
+size 1674
--- FreeflowCombat/Assets/Scripts/Animation/AttackAnimationInfo.cs
+++ FreeflowCombat/Assets/Scripts/Animation/AttackAnimationInfo.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6407b368b2e65c7166f35d2656b90464d00e7d9f664f1bc8efef585d480322ca
-size 925
+oid sha256:b0c1a145359a85c827fc00938a5dd754ed5343fb2a54491f6bcbdbd61bd89f5a
+size 1434
