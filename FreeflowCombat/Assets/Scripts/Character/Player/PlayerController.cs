c6f11d912da17acf956050cf93659cde700e44e8
Omurex <josephthomaslyons@gmail.com>
2023-05-14 21:45:50 -0400

    Added odin stuff to help development, brainstorming how to makeattack animations distance dependent

--- FreeflowCombat/Assets/Scripts/Character/Player/PlayerController.cs
+++ FreeflowCombat/Assets/Scripts/Character/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:959c03920fa96607ddd6ad128abae0c7f101d4e7a73dd0b05c8eec3d85d213ab
-size 15906
+oid sha256:b06e0fcf711554701a98326f00a4cce7fe2c37444125679fd3a762745a332978
+size 16312
