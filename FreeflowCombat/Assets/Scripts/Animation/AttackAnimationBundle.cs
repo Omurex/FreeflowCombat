33e3fc7d19612db718f1e4fcfeb9554384e4b4da
Omurex <josephthomaslyons@gmail.com>
2023-05-15 19:00:57 -0400

    Added new attack animation: elbow hit

--- FreeflowCombat/Assets/Scripts/Animation/AttackAnimationBundle.cs
+++ FreeflowCombat/Assets/Scripts/Animation/AttackAnimationBundle.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:537e182b595d1247c71a606c7390e13c00df4150deaea9377099d90ee6c42e05
-size 1674
+oid sha256:edc90c2860f4438133c785ef9b1f28ee6122400d150c9a85d3338fb251d8ae57
+size 1612
---
+++ FreeflowCombat/Assets/Scripts/Test/Test_LogDistance.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:40605b000383c164ac0c5e3221b53def9117b36b9013ccac09a3f9dced63e235
+size 580
